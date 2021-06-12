5b86fbc23d4578196f2ff6ca325c9ec89c648a45
Ivan Valdes <ivaldes@modyo.com>
2021-06-12 14:05:39 -0400

    recursion topic search

--- app/models/member.rb
+++ app/models/member.rb
@@ -8,4 +8,41 @@ class Member < ApplicationRecord
             break unless Member.where(url_slug: url_slug).exists?
         end
     end
+
+    def friends
+        fs = Friendship
+             .where("member_id = ? OR friend_id = ?", self.id, self.id)
+             .pluck(:member_id, :friend_id).flatten - [self.id]
+        Member.where(id: fs)
+    end
+
+    def find_expert(topic)
+        headers = Header.where("header LIKE '%#{topic}%'").where.not(member: self).where.not(member: self.friends)
+        return nil unless headers.any?
+
+        headers.map do |header|
+            route = find_friend(header.member, [])
+            return nil unless route
+            
+            return {route: route, header: header}
+        end
+    end
+
+    def is_friend?(member)
+        self.friends.include?(member)
+    end
+
+    # Recursive function to return route to author
+    def find_friend(member, route)
+        return nil if route.include?(self)
+
+        r = route.dup.append(self)
+        return r if is_friend?(member)
+
+        friends.each do |f|
+            found_route = f.find_friend(member, r)
+            return found_route unless found_route.nil?
+        end
+        return nil
+    end
 end
--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Member, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'finds topic route' do
+    [Header, Friendship, Member].each { |m| m.destroy_all }
+    5.times do |i|
+      m = Member.create(first_name: "#{i}_firstname", last_name: "#{i}_lastname", url: "#{i}dummyUrl")
+      next if i.zero?
+
+      Friendship.create(member_id: m.id, friend_id: m.id - 1)
+    end
+    Header.create(member: Member.last, header: 'this superTopic is powerful')
+    expect Member.first.find_expert('superTopic')[:route].last.id == Member.last.id 
+  end
 end
